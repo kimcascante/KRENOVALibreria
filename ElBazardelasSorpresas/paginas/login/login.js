1052014b190b7b015ee88d729db3a88685151555
Luis Diego Herrera <lherreram1@ucenfotec.ac.cr>
2022-04-03 16:46:49 -0600

    login
    agrego primera version de login

--- ElBazardelasSorpresas/paginas/login/login.js
+++ ElBazardelasSorpresas/paginas/login/login.js
@@ -1,1 +1,16 @@
-alert('mi login')
+const form = document.getElementById('loginForm');
+
+function onSubmit(event){
+    console.log(event)
+    event.preventDefault();
+    //VAlidaciones
+    const passField = document.getElementById("pass");
+
+    if (passField.value == ""){
+        alert("agregue contra")
+    }
+    
+}
+
+// form.addEventListener('submit',onSubmit)
+// form.addEventListener('click',onSubmit)
