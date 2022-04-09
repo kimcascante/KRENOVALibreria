694f5d7800edb85bc97ff3b5fba7d7d7a070aa50
kimcascante <72054684+kimcascante@users.noreply.github.com>
2022-04-09 01:08:23 -0600

    inicio de sesion validado
    inicio de sesion validado

--- ElBazardelasSorpresas/js/login.js
+++ ElBazardelasSorpresas/js/login.js
@@ -1,16 +1,66 @@
-const form = document.getElementById('loginForm');
+//Constantes para extraer datos del formulario HTML
+const botonRegistrar = document.getElementById('btn-ingresar');
+const inputCorreo = document.getElementById('txt-correo');
+const inputContrasenna = document.getElementById('txt-contrasenna');
 
-function onSubmit(event) {
-    console.log(event)
-    event.preventDefault();
-    //VAlidaciones
-    const passField = document.getElementById("pass");
+const imprimir = () => {
+    let correo = inputCorreo.value;
+    let contrasenna = inputContrasenna.value;
 
-    if (passField.value == "") {
-        alert("agregue contra")
+    console.log('El correo es:', correo);
+    console.log('La contraseña es:', contrasenna);
+
+};
+
+
+// Crear función de validación "validar"
+const validar = () => {
+    let hayError = false;
+
+    if (inputCorreo.value == '') {
+        hayError = true;
+    } else {
+
     }
 
+    if (inputContrasenna.value == '') {
+        hayError = true;
+    }
+
+    //Validación final
+    if (hayError) {
+        Swal.fire({
+            'icon': 'warning',
+            'title': 'Inicio de sesión fallido',
+            'text': 'Usuario o contraseña inválidos'
+        });
+    } else {
+        /*Swal.fire({
+            'icon': 'success',
+            'title': 'Inicio de sesión exitoso',
+            'text': 'Bienvenido al Bazar de las Sorpresas!' * /
+        })*/
+        /*.then(() => {*/
+        //Redirecciona al landing page del Bazar
+
+        if (inputCorreo.value == 'jmadriz@gmail.com' && inputContrasenna.value == 'madriz123') {
+            window.location.href = 'landingPageBazarRegistrado.html'
+
+        } else if (inputCorreo.value == 'pcastillo@gmail.com' && inputContrasenna.value == 'paola123') {
+            window.location.href = 'landingPageBazarAdministrador.html'
+
+        } else {
+            Swal.fire({
+                'icon': 'warning',
+                'title': 'Inicio de sesión fallido',
+                'text': 'Usuario o contraseña inválidos'
+            })
+        }
+
+        /* });*/
+    };
 }
 
-// form.addEventListener('submit',onSubmit)
-// form.addEventListener('click',onSubmit)
+//imprimir();
+
+botonRegistrar.addEventListener('click', validar);
