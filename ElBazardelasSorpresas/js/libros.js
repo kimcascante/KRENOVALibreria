efcd114c7327420eedc4744bcab1cef2c1b3d8e6
kimcascante <72054684+kimcascante@users.noreply.github.com>
2022-04-10 14:42:21 -0600

    mas libros al listado
    mas libros al listado

--- ElBazardelasSorpresas/js/libros.js
+++ ElBazardelasSorpresas/js/libros.js
@@ -7,14 +7,49 @@ let listaLibros = [{
 
 }, {
     'isbn': 9452584563578,
-    'nombreLibro': 'YO SERÉ LA ÚLTIMA',
+    'nombreLibro': 'Yo seré la última',
     'autor': 'Nadia Murad',
     'inventarioInicial': '30'
 }, {
-    'isbn': 9458524569519,
-    'nombreLibro': 'TORTURA:EL CRIMEN DE COLIMA',
+    'isbn': 9789977655201,
+    'nombreLibro': 'Tortura: El Crimen de Colima',
     'autor': 'José León Sánchez',
     'inventarioInicial': '50'
+}, {
+    'isbn': 9789968468268,
+    'nombreLibro': 'TENOCHTITLAN',
+    'autor': 'José León Sánchez',
+    'inventarioInicial': '20'
+}, {
+    'isbn': 9788466357326,
+    'nombreLibro': 'Doctor sueño',
+    'autor': 'Stephen King',
+    'inventarioInicial': '20'
+}, {
+    'isbn': 9788466345453,
+    'nombreLibro': 'Fin de guardia',
+    'autor': 'Stephen King',
+    'inventarioInicial': '20'
+}, {
+    'isbn': 9788490627662,
+    'nombreLibro': 'Mr Mercedes',
+    'autor': 'Stephen King',
+    'inventarioInicial': '20'
+}, {
+    'isbn': 9786073150392,
+    'nombreLibro': 'Quien pierde gana',
+    'autor': 'Stephen King',
+    'inventarioInicial': '20'
+}, {
+    'isbn': 9789977239279,
+    'nombreLibro': 'Mamita Yunai',
+    'autor': 'Carlos Luis Fallas',
+    'inventarioInicial': '20'
+}, {
+    'isbn': 9788413141879,
+    'nombreLibro': 'Divergente',
+    'autor': 'Veronica Roth',
+    'inventarioInicial': '20'
 }];
 
 // console.log('--- Ciclo forEach ---');
