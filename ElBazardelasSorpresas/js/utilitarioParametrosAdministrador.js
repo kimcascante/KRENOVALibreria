570db88f06af38e51700b1ffea475416c8d90967
rabasalsa <102842316+rabasalsa@users.noreply.github.com>
2022-04-11 17:05:37 -0600

    parametros
    paramertros

---
+++ ElBazardelasSorpresas/js/utilitarioParametrosAdministrador.js
@@ -0,0 +1,63 @@
+const inputImpuesto = document.getElementById('txt-idimpuesto');
+const inputNombre = document.getElementById('txt-nombreImpuesto');
+const inputPorcentaje = document.getElementById('txt-porcentajeImpuesto');
+const inputEstado = document.getElementById('slt-genero');
+const botonGuardar = document.getElementById('btn-registrar');
+
+
+const validar = () => {
+    let hayError = false;
+
+    if (inputImpuesto.value == '') {
+        hayError = true;
+        inputImpuesto.classList.add('input-error')
+    } else {
+        inputImpuesto.classList.remove('input-error');
+    }
+
+    if (inputNombre.value == '') {
+        hayError = true;
+        inputNombre.classList.add('input-error')
+    } else {
+        inputNombre.classList.remove('input-error');
+    }
+
+    if (inputPorcentaje.value == '') {
+        hayError = true;
+        inputPorcentaje.classList.add('input-error')
+    } else {
+        inputPorcentaje.classList.remove('input-error');
+    }
+
+    if (inputEstado.value == '') {
+        hayError = true;
+        inputEstado.classList.add('input-error')
+    } else {
+        inputEstado.classList.remove('input-error');
+    }
+
+
+    //validacion final
+
+    if (hayError == true) {
+
+        Swal.fire({
+            icon: 'error',
+            title: '',
+            text: 'Por favor llene todos los campos',
+        })
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Datos Guardados',
+            text: '',
+        }).then(() => {
+            //redirecciona 
+
+        });
+    }
+};
+
+
+//console clear
+botonGuardar.addEventListener('click', validar);
