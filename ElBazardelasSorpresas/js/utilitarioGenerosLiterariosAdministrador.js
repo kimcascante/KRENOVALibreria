d1631509b609f746b2760cf3bfca8ab8fbdfcedf
CALVERDE22 <101017624+CALVERDE22@users.noreply.github.com>
2022-04-10 22:49:58 -0600

    Validación de géneros literarios
    Validación de géneros literarios

---
+++ ElBazardelasSorpresas/js/utilitarioGenerosLiterariosAdministrador.js
@@ -0,0 +1,45 @@
+const botonRegistrar = document.getElementById('btn-registrar');
+const inputGeneroLiterario = document.getElementById('txt-idGenero');
+const inputNombreGenero = document.getElementById('txt-nombreGenero');
+
+//Función de validación
+const validar = () => {
+    let hayError = false;
+
+    if (inputGeneroLiterario.value == '') {
+        hayError = true;
+    } else {
+
+    }
+
+    if (inputNombreGenero.value == '') {
+        hayError = true;
+    }
+
+    //Validacion final
+    if (hayError) {
+        Swal.fire({
+            'icon': 'warning',
+            'title': 'Género no registrado',
+            'text': 'Favor complete todos los campos'
+        });
+    } else {
+        Swal.fire({
+            'icon': 'success',
+            'title': 'Género  registrado',
+            'text': 'Registro completado!'
+        }).then(() => {
+            //Redirecciona al listado de autores
+            window.location.href = 'generosliterarios-administrador.html'
+        });
+
+    };
+
+
+}
+
+
+
+
+
+botonRegistrar.addEventListener('click', validar);
