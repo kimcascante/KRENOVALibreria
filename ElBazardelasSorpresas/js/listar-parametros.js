032f2bfa597ef047a0441a72451dce4fc6eee151
JKLdoce <jlopezm@ucenfotec.ac.cr>
2022-04-10 23:22:13 -0600

    Listar y registrar parametros
    Listar y registrar tarifas de envío
    Listar y registrar impuestos

---
+++ ElBazardelasSorpresas/js/listar-parametros.js
@@ -0,0 +1,23 @@
+const cuerpoTabla = document.querySelector('#tbl-listarImpuestos tbody');
+
+
+
+const mostrarDatos = () => {
+    // Limpiar el cuerpo de la tabla
+    cuerpoTabla.innerHTML = '';
+
+
+    listaImpuestos.forEach(impuesto => {
+        let fila = cuerpoTabla.insertRow();
+
+        fila.insertCell().textContent = impuesto.idimpuesto;
+        fila.insertCell().textContent = impuesto.nombreimpuesto;
+        fila.insertCell().textContent = impuesto.porcentaje;
+        fila.insertCell().textContent = impuesto.estado;
+
+    });
+
+
+};
+
+mostrarDatos();
---
+++ ElBazardelasSorpresas/js/parametros.js
@@ -0,0 +1,12 @@
+let listaImpuestos = [{
+    'idimpuesto': 2040,
+    'nombreimpuesto': 'IVA',
+    'porcentaje': 13,
+    'estado': 'Activo'
+
+}, {
+    'idimpuesto': 2041,
+    'nombreimpuesto': 'EXENTO',
+    'porcentaje': 0,
+    'estado': 'Activo'
+}];
