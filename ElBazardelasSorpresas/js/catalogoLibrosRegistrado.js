3581b2de161d1e7ffed2142dda401f37edfa2c01
JKLdoce <jlopezm@ucenfotec.ac.cr>
2022-05-05 11:08:32 -0600

    Carrito Compras Agregar Items
    Carrito Compras Agregar Items

--- ElBazardelasSorpresas/js/catalogoLibrosRegistrado.js
+++ ElBazardelasSorpresas/js/catalogoLibrosRegistrado.js
@@ -1,5 +1,8 @@
 const addToShoppingCartButtons = document.querySelectorAll('.dv-button')
-console.log(addToShoppingCartButtons);
+const shoppingCarItem = document.querySelector('.shoppingCarItemsContainer');
+//const shoppingCarItem = document.getElementById('contenidoCarrito');
+//console.log(addToShoppingCartButtons);
+
 addToShoppingCartButtons.forEach((addToCartButton) => {
     addToCartButton.addEventListener('click', addToCartClicked);
 });
@@ -11,13 +14,47 @@ function addToCartClicked(event) {
     const itemImagen = item.querySelector('.lb-imagen').src;
     const itemTitulo = item.querySelector('.lb-title').textContent;
     const itemPrecio = item.querySelector('.lb-precio').textContent;
+    //console.log("Click", itemImagen, itemTitulo, itemPrecio);
 
     addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio);
 
 }
 
 function addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio) {
+    //console.log('funcion', itemImagen, itemTitulo, itemPrecio);
     const shoppingCartRow = document.createElement('div');
+    /*const shoppingCartContent = `
+          <div class="shoppingCart">
+           <img class="fotoLibroCarrito" src=${itemImagen}>
+           <p class="lb-nombreAutor" for="">${itemTitulo}</p>
+           <p class="lb-precio" for="">${itemPrecio}</p>
+        </div>`;*/
+    const shoppingCartContent = itemImagen + itemTitulo + itemPrecio;
 
-    console.log(itemImagen, itemTitulo, itemPrecio);
+    //console.log('variable', shoppingCartContent);
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    //console.log('shoppingCartContent', shoppingCartContent);
+    //console.log('shoppingCartRow', shoppingCartRow);
+
+    shoppingCarItem.append(shoppingCartRow);
+    console.log('Arreglo', shoppingCarItem);
+
+    updateShoppingCarTotal();
+
+
+
+}
+
+function updateShoppingCarTotal() {
+    let total = 0;
+    const shoppingCarTotal = document.querySelector('.shoppingCarTotal');
+    const shoppingCarItems = document.querySelectorAll('.shoppingCarItemsContainer');
+    console.log('Contenidocarrito', shoppingCarItems);
+
+    shoppingCarItems.forEach(shoppingCarItem => {
+        const shoppingCartItemPriceElement = shoppingCarItem.querySelector('.shoppingCartItemsContainer');
+
+        console.log('Elemento', shoppingCartItemPriceElement);
+
+    });
 }
