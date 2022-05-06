95210f9225abb25ccd956aed9ce04f388452956b
JKLdoce <jlopezm@ucenfotec.ac.cr>
2022-05-06 10:56:19 -0600

    Carrito de compras finalizado
    Carrito de compras finalizado

--- ElBazardelasSorpresas/js/catalogoLibrosRegistrado.js
+++ ElBazardelasSorpresas/js/catalogoLibrosRegistrado.js
@@ -1,5 +1,6 @@
 const addToShoppingCartButtons = document.querySelectorAll('.dv-button')
 const shoppingCarItem = document.querySelector('.shoppingCarItemsContainer');
+//const containerCart = document.querySelector('.item-shoppingCar');
 let carritoVacio = 0;
 let cantidadActual = 0;
 //const shoppingCarItem = document.getElementById('contenidoCarrito');
@@ -20,11 +21,14 @@ function addToCartClicked(event) {
     //console.log("Click", itemImagen, itemTitulo, itemPrecio);
 
     addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio, itemCantidad);
+    //updateShoppingCarTotal();
 
 }
 
 function addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio, itemCantidad) {
-    //console.log('funcion', itemImagen, itemTitulo, itemPrecio);
+
+    //const elementsTitle = shoppingCarItem.getElementsByClassName('.item-shoppingCar');
+    //console.log('funcionNo Repita', elementsTitle);
 
     //  let insertarItem = 0;
     //const addshoppingCarItems = document.querySelectorAll('.shoppingCarItemsContainer');
@@ -57,7 +61,7 @@ function addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio, itemCantidad)
                                                     <label>Precio</label>
                                                     <label class="lb-precio" for="">${itemPrecio}</label>
                                                     <label>Cantidad</label>
-                                                    <input type= "number" class="txt-cantidad" for="" min="1" value =1></input>
+                                                    <input type= "number" weight= "40"class="txt-cantidad" for="" min="1" value =1></input>
                                                     </div>`;
 
             /*const shoppingCartContent = `
@@ -80,15 +84,40 @@ function addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio, itemCantidad)
     } else {
 */
     const shoppingCartRow = document.createElement('div');
-    const shoppingCartContent = `
-                                                <div class="itemLibro">
-                                                <a href="perfilMamitaYunai.html"><img class="lb-imagen" src=${itemImagen} alt="" height="250" width="200"></a>
-                                                <label class="lb-title" for="">${itemTitulo}</label>
+    const shoppingCartContent = `     <div class="item-shoppingCar">                                          
+                                                <div class="columna-imagen">
+                                                <div class="lb-imagen">
+                                                <img  src=${itemImagen} class="Imagenlibro-carrito" alt="" height="80" width="80">
+                                                </div>
+                                                </div>
+                                                <div class="columna-titulo">
+                                                <label>Titulo</label>
+                                                <div class="lb-title" >${itemTitulo}</div>
+                                                </div>
+                                                <div class="columna-precio">
                                                 <label>Precio</label>
-                                                <label class="lb-precio" for="">${itemPrecio}</label>
+                                                <div class="lb-precio">${itemPrecio}</div>
+                                                </div>
+                                                <div class="columna-cantidad">
                                                 <label>Cantidad</label>
-                                                <input type= "number" class="txt-cantidad" for="" min="1" value =1></input>
-                                                </div>`;
+                                                <div class="txt-cantidad">
+                                                <input class="cantidad-carrito" type= "number" size=40 style="with:1px; heigth:1px" min="1" value="1"></input>
+                                                <button class="btn-eliminar" type="button">Eliminar</button>
+                                                </div>
+                                                </div>
+                                        </div>`;
+
+    /* `
+    <div class="itemLibro">
+    <a href="perfilMamitaYunai.html"><img class="lb-imagen" src=${itemImagen} alt="" height="250" width="200"></a>
+    <label class="lb-title" for="">${itemTitulo}</label>
+    <label>Precio</label>
+    <label class="lb-precio" for="">${itemPrecio}</label>
+    <label>Cantidad</label>
+    <input type= "number" size=40 style="with:1px; heigth:1px" class="txt-cantidad" for="" min="1" value =1></input>
+     <input class="cantidad-carrito" type= "number" size=40 style="with:1px; heigth:1px" min="1" value="1"></input>
+     <input class="cantidad-carrito" type= "number" size=40 style="with:1px; heigth:1px" min="1" value="1" disabled></input>
+    </div>`;*/
 
     /*const shoppingCartContent = `
         <div class="shoppingCart">
@@ -101,10 +130,13 @@ function addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio, itemCantidad)
     //console.log('variable', shoppingCartContent);
     shoppingCartRow.innerHTML = shoppingCartContent;
     //console.log('shoppingCartContent', shoppingCartContent);
-    console.log('shoppingCartRow', shoppingCartRow);
+    //console.log('shoppingCartRow', shoppingCartRow);
 
     shoppingCarItem.append(shoppingCartRow);
+    shoppingCartRow.querySelector('.btn-eliminar').addEventListener('click', removeShoppingCartItem);
+    shoppingCartRow.querySelector('.cantidad-carrito').addEventListener('change', cantidadCompra);
 
+    updateShoppingCarTotal()
 
 
 
@@ -112,7 +144,7 @@ function addItemToShoppingCart(itemImagen, itemTitulo, itemPrecio, itemCantidad)
     // }
 
 
-    updateShoppingCarTotal();
+
 }
 
 
@@ -121,24 +153,50 @@ function updateShoppingCarTotal() {
     let total = 0;
 
     const shoppingCarTotal = document.querySelector('.shoppingCarTotal');
-    const shoppingCarItems = document.querySelectorAll('.shoppingCarItemsContainer');
-    //console.log('Contenidocarrito', shoppingCarItems);
+    //const shoppingCarItems = document.querySelectorAll('.shoppingCarItemsContainer');
+    const shoppingCarItems = document.querySelectorAll('.item-shoppingCar');
+    console.log('Contenidocarrito', shoppingCarItems);
 
     shoppingCarItems.forEach(shoppingCarItem => {
+        //console.log('Arregloshopping', shoppingCarItem);
+        /*const shoppingCartItemElement = shoppingCarItem.querySelector('.itemLibro');
+        console.log('Elementoshopping', shoppingCartItemElement);*/
+        //const shoppingCarItemAll = shoppingCarItem.querySelector('.item-shoppingCar');
+        //console.log('Elementoseparado', shoppingCarItemAll);
         const shoppingCartItemPriceElement = shoppingCarItem.querySelector('.lb-precio');
         const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('₡', ''));
-        const shoppingCartItemQuantityElement = shoppingCarItem.querySelector('.txt-cantidad')
+
+        const shoppingCartItemQuantityElement = shoppingCarItem.querySelector('.cantidad-carrito')
         const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
 
-        console.log('PrecioElemento', shoppingCartItemPriceElement);
-        //total = total + (shoppingCartItemPrice * shoppingCartItemQuantity);
 
-        console.log('Total', shoppingCartItemPrice);
+
+
+        //console.log('PrecioElemento', shoppingCartItemQuantity);
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+
+        console.log('PrecioTotal', total);
         //console.log('Elemento', shoppingCartItemQuantity);
         //console.log('Precio', shoppingCartItemPriceElement);
 
     });
 
+    shoppingCarTotal.innerHTML = `<label>Precio Total ¢</label>${total.toFixed(2)}`
 
+
+
+}
+
+function removeShoppingCartItem(event) {
+    const buttonClicked = event.target;
+    buttonClicked.closest('.item-shoppingCar').remove();
+    updateShoppingCarTotal();
+
+}
+
+function cantidadCompra(event) {
+    const inputCantidad = event.target;
+    inputCantidad.value <= 0 ? (input.value = 1) : null;
+    updateShoppingCarTotal();
 
 }
