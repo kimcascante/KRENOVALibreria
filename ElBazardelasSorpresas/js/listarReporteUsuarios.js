2cb0b6d49a84aec0c674b0d619bf4beef20889f1
kimcascante <72054684+kimcascante@users.noreply.github.com>
2022-05-06 15:21:56 -0600

    buscar usuarios
    buscar usuarios

--- ElBazardelasSorpresas/js/listarReporteUsuarios.js
+++ ElBazardelasSorpresas/js/listarReporteUsuarios.js
@@ -10,22 +10,27 @@ const inicializar = async() => {
 const mostrarDatos = () => {
     // Limpiar el cuerpo de la tabla
     cuerpoTabla.innerHTML = '';
+    let filtro = document.getElementById('txt-filtroUsuario').value.toLowerCase();
 
 
     listarUsuario.forEach(usuario => {
-        let fila = cuerpoTabla.insertRow();
+        if (usuario.primerNombre.toLowerCase().includes(filtro)) {
 
-        fila.insertCell().textContent = usuario.correoElectronico;
-        fila.insertCell().textContent = usuario.primerNombre;
-        fila.insertCell().textContent = usuario.primerApellido;
-        fila.insertCell().textContent = usuario.identificacion;
-        fila.insertCell().textContent = usuario.direccion;
+            let fila = cuerpoTabla.insertRow();
 
+            fila.insertCell().textContent = usuario.correoElectronico;
+            fila.insertCell().textContent = usuario.primerNombre;
+            fila.insertCell().textContent = usuario.primerApellido;
+            fila.insertCell().textContent = usuario.identificacion;
+            fila.insertCell().textContent = usuario.direccion;
 
+        }
     });
 
 
 };
 
 inicializar();
+
+document.getElementById('txt-filtroUsuario').addEventListener('keyup', mostrarDatos);
 //mostrarDatos();
